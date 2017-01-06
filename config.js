b24bf1f98e85a83b18ced33d7b9f2a3011380418
Shurugaz <edwardlecock@gmail.com>
2017-01-06 14:05:52 +0100

    Ready to deploy

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 
 	// Your bot name. Typically, this is your bot's username without the discriminator.
 	// i.e: if your bot's username is MemeBot#0420, then this option would be MemeBot.
-	name: "ScheduleBot",
+    name: "iScheduleDoto",
 
 	// The bot's command prefix. The bot will recognize as command any message that begins with it.
 	// i.e: "-schedulebot foo" will trigger the command "foo",
@@ -11,7 +11,7 @@ module.exports = {
 	// If you replace YOUR_BOT_USER_ID with your bot's user ID, the prefix will be a mention to
 	// your bot. You can get that ID in https://discordapp.com/developers/applications/me/
 	// (click on your application, and find it under "App Details" > "Client ID"
-	prefix:  "<@YOUR_BOT_USER_ID>",
+    prefix:  "-schedule",
 
 	// This is a readable version of the prefix. Generally, this is the same as prefix, but if
 	// you set prefix to be in the form of "<@YOUR_BOT_USER_ID>", you'd need to set readable_prefix
@@ -21,7 +21,7 @@ module.exports = {
 	// ScheduleBot's case), the string "<@YOUR_BOT_USER_ID>" doesn't get parsed as a mention.
 	// So in order not to mislead the user, we have a separate option for a readable version of
 	// our prefix.
-	readable_prefix: "@YOUR_BOT_USER_NAME",
+    readable_prefix: "-schedule",
 
 	// Admin app settings
 	admin_app: {
@@ -31,7 +31,7 @@ module.exports = {
 
 	// The master channel
 	// The bot will announce the events to this channel. It won't listen to other channels.
-	master_channel: "YOUR_MASTER_CHANNEL",
+    master_channel: "265939233452064768",
 
 	// Events are considered "happening" for a margin of time, where users can see that the event
 	// is happening right now. During that time, the event is not considered expired yet.+
@@ -87,11 +87,11 @@ module.exports = {
 	},
 
 	steam: {
-		// The name that the Steam bot will take
-		name: "ScheduleBot",
+        // The name that the Steam bot will take
+        name: "ScheduleBot",
 
-		// The bot's profile URL. It's needed to redirect users to it.
-		profile_url: "http://steamcommunity.com/profiles/YOUR_BOT_ID/"
+        // The bot's profile URL. It's needed to redirect users to it.
+        profile_url: "http://steamcommunity.com/profiles/ischeduledoto/"
 	},
 
 	dota: {
